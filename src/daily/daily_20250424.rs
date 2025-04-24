9f11572c8b7eaf1867ee5a265f7ec677c4dd4e39
ligt <me@ligt.dev>
2025-04-24 07:57:13 +0700

    more option for potd

--- src/daily/daily_20250424.rs
+++ src/daily/daily_20250424.rs
@@ -2,7 +2,7 @@ use std::collections::{HashMap, HashSet};
 
 impl Solution {
     pub fn count_complete_subarrays(nums: Vec<i32>) -> i32 {
-        // ######### get unique num #########
+        // ######### get unique num by sorting #########
         let mut sorted_nums = nums.clone();
         sorted_nums.sort_unstable();
         let (_, cnt_uniq) = sorted_nums.iter().fold((0, 0), |(last_value, cnt), &item| {
@@ -11,6 +11,10 @@ impl Solution {
             }
             (item, cnt + 1)
         });
+
+        // ########## Get by using hashset ##############
+        let cnt_uniq = nums.iter().collect::<HashSet<&i32>>().len();
+
         // ######### the brute force way #########
         let mut ans = 0;
         let n = nums.len();
@@ -23,6 +27,30 @@ impl Solution {
                 }
             }
         }
+
+        // ######### the sliding way ###########
+        let mut ans = 0;
+        let n = nums.len();
+        let mut right = 0;
+        let mut hash_table = HashMap::new();
+        for left in 0..n {
+            while right < n && hash_table.len() < cnt_uniq {
+                hash_table
+                    .entry(nums[right])
+                    .and_modify(|e| *e += 1)
+                    .or_insert(1);
+                right += 1;
+            }
+            if hash_table.len() < cnt_uniq {
+                break;
+            }
+            ans += n as i32 - right as i32 + 1;
+            hash_table.entry(nums[left]).and_modify(|e| *e -= 1);
+            if hash_table.get(&nums[left]).unwrap() == &0 {
+                hash_table.remove(&nums[left]);
+            }
+        }
+
         ans
     }
 }
